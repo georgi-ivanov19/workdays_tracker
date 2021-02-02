40fa9fdfab6c2cb361618aa22944901652585f29
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2021-02-02 12:38:20 +0200

    modified main activity and defined a Date model

--- app/src/main/java/com/example/workdaystracker/MainActivity.java
+++ app/src/main/java/com/example/workdaystracker/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.workdaystracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView datesRecyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        datesRecyclerView=findViewById(R.id.datesRecyclerView);
+        datesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ app/src/main/java/com/example/workdaystracker/Model/DateModel.java
@@ -0,0 +1,30 @@
+package com.example.workdaystracker.Model;
+
+public class DateModel {
+    private int id, status;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
